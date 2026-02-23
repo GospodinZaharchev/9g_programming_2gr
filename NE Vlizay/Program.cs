20f0bfafe38bbc6e129b859eff706da0357f5fb7
Vaseto2011 <vasil.v.popov2011@gmail.com>
2026-02-23 11:34:39 +0200

    Add files via upload

---
+++ NE Vlizay/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel.Design;
+
+namespace ConsoleApp365
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int a = int.Parse(Console.ReadLine());
+            if (a == 1)
+            {
+                Console.WriteLine(word);
+            }
+            else if (a == 2)
+            {
+                Console.WriteLine(word + word);
+            }
+            else if (a == 3)
+            {
+                Console.WriteLine(word + word + word);
+            }
+            else if (a == 4)
+            {
+                Console.WriteLine(word + word + word + word);
+            }
+            else if
+              (a == 5)
+            {
+                Console.WriteLine(word + word + word + word + word);
+            }
+            else if (a == 6)
+            {
+                Console.WriteLine(word + word + word + word + word + word);
+            }
+            else if (a == 7)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word);
+            }
+            else if (a == 8)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word);
+            }
+            else if (a == 9)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word + word);
+            }
+            else if (a == 10)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word + word + word);
+            }
+            else if (a == 11)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word + word + word + word);
+            }
+            else if (a == 12)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word + word + word + word + word);
+            }
+            else if (a == 13)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word + word + word + word + word + word);
+            }
+            else if (a == 14)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word + word + word + word + word + word + word);
+            }
+            else if (a == 15)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word + word + word + word + word + word + word + word);
+            }
+            else if (a == 16)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word + word + word + word + word + word + word + word + word);
+            }
+            else if (a == 17)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word + word + word + word + word + word + word + word + word + word);
+            }
+        }
+    }
+}
