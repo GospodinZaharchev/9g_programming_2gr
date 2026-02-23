7f044ba96200fc85ab90d3f2cc2c064927c6fe8d
bu11et-hell <plamenborislav@gmail.com>
2026-02-23 11:25:46 +0200

    Добавена задача 5

---
+++ repeatstring/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace repeatstring
+{
+    internal class Program
+    {
+        static void StringRepeater(int timesRepeat, string wordToBeRepeated)
+        {
+            for (int i = 0; i < timesRepeat; i++)
+            {
+                Console.Write(wordToBeRepeated);
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how many time?");
+            int timesReapeat = int.Parse(Console.ReadLine());
+            Console.WriteLine("what to repeat?");
+            string wordToBeRepeated = Console.ReadLine();
+
+            StringRepeater(timesReapeat, wordToBeRepeated);
+        }
+    }
+}
