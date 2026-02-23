040f17bbd808230aa4203bc32124be2494737246
Yoyo-2011-yp <yoana.popova2011@gmail.com>
2026-02-23 11:37:39 +0200

    Add files via upload

---
+++ Yoana.9g/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace ConsoleApp363
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            {
+                static void Print(string word)
+                {
+                    Console.WriteLine(word);
+                }
+                {
+                    Console.WriteLine("Enter a word:");
+                }
+
+            }
+
+            string word = Console.ReadLine();
+            int a = int.Parse(Console.ReadLine());
+            if (a == 1)
+            {
+                Console.WriteLine(word);
+            }
+            else if (a == 2)
+            {
+                Console.WriteLine(word + word);
+            }
+            else if (a == 3)
+            {
+                Console.WriteLine(word + word + word);
+            }
+            else if (a == 4)
+            {
+                Console.WriteLine(word + word + word + word);
+            }
+            else if (a == 5)
+            {
+                Console.WriteLine(word + word + word + word + word);
+            }
+            else if (a == 6)
+            {
+                Console.WriteLine(word + word + word + word + word + word);
+            }
+            else if (a == 7)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word);
+            }
+            else if (a == 8)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word);
+            }
+            else if (a == 9)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word + word);
+            }
+            else if (a == 10)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word + word + word);
+            }
+            else if (a == 11)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word + word + word + word);
+            }
+            else if (a == 12)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word + word + word + word + word);
+            }
+            else if (a == 13)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word + word + word + word + word + word);
+            }
+            else if (a == 14)
+            {
+                Console.WriteLine(word + word + word + word + word + word + word + word + word + word + word + word + word + word);
+            }
+
+
+
+
+
+        }
+
+
+        
+    }
+}
