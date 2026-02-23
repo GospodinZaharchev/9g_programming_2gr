5282c63a60651fceb20c31b2b047e4d13c2b828e
bu11et-hell <plamenborislav@gmail.com>
2026-02-23 11:45:19 +0200

    задача 6

---
+++ Pln_fldr/numfact/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace numfact
+{
+    internal class Program
+    {
+        static int NumFact(int number, int fact)
+        {
+            int buffer = number;
+            for (int i = 0; i < fact; i++)
+            {
+                number = number * buffer;
+            }
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how many times?");
+            int fact = int.Parse(Console.ReadLine());
+            Console.WriteLine("what to repeat?");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(NumFact(number, fact));
+        }
+    }
+}
